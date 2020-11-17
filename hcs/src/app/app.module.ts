5ff22c81a7d31cd68bfdda3a9664f18ee409a5b8
GabrielaGaspar <72529661+GabiGaspar@users.noreply.github.com>
2020-11-17 17:19:05 -0300

    login
    tela de login e service

--- hcs/src/app/app.module.ts
+++ hcs/src/app/app.module.ts
@@ -1,18 +1,29 @@
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './pages/login/login.component';
+import { MaterialModule } from './material.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    FormsModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
