cbc2576f2e2418f3682792738ff1b93ca98ae999
GabrielaGaspar <72529661+GabiGaspar@users.noreply.github.com>
2020-11-26 09:27:04 -0300

    Tabela de Experiência
    Função Editar Tabela Criada e mensagems de alerta de sucesso no envio, edição  e atualização da tabela. (sem layout)

--- hcs/src/app/pages/servers/add-exp-table/add-exp-table.component.ts
+++ hcs/src/app/pages/servers/add-exp-table/add-exp-table.component.ts
@@ -26,27 +26,50 @@ export class AddExpTableComponent implements OnInit {
 
   }
 
-
   ngOnInit(): void {
   }
   model: any={};
+  model2: any={};
   expTab: any=[];
+  msg:any="";
   addrowTable(){
     this.experience.push(this.model);
     console.log(this.experience)
     this.model = {};
     this.expTab = this.experience;
+    this.msg = "Item adicionado com sucesso"
 
   }
-
-
   saveExpTable(){
-  return this.addExpTable.editExpTable(this.expTab).subscribe(res=>console.log(res)) }
+  return this.addExpTable.editExpTable(this.expTab).subscribe(res=>console.log(res));
+
+ }
 
   delrowTable(i){
     this.experience.splice(i,1)
     console.log(i);
+    this.msg = "Item deletado com sucesso"
+  }
+  myValue;
+  editrowTable(k){
+    this.model2.title = this.experience[k].title;
+    this.model2.level = this.experience[k].level;
+    this.model2.exp = this.experience[k].exp;
+    this.myValue = k;
+
+  }
+  updateRow(){
+    let k= this.myValue;
+    for(let i=0; i<this.experience.length; i++){
+          if(i==k){
+            this.experience[i]=this.model2;
+            this.model2 ={};
+          }
+    }
+    this.msg = "Item atualizado com sucesso"
   }
 
-
+  clickme(){
+    this.msg="";
+  }
 }
